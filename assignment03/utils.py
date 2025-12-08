3ebff0fb544f89079046903d0aa696d6f310e60a
unknown <ramon.naef@students.unibe.ch>
2025-12-08 11:11:38 +0100

    Assignment 3 finished

--- assignment03/utils.py
+++ assignment03/utils.py
@@ -1,7 +1,11 @@
+#Ramon Näf; 20-116-950
+
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.sparse as sparse
 from scipy.sparse.linalg import lsmr, svds
+from scipy.sparse import diags
+import cv2
 
 def get_normalization_matrix(x):
     """
@@ -21,7 +25,30 @@ def get_normalization_matrix(x):
     # --------------------------------------------------------------
 
     # Get centroid and mean-distance to centroid
-    T = None
+    x = x.copy().astype(float)
+    x /= x[2, :] 
+
+    u = x[0, :]
+    v = x[1, :]
+
+    cx = np.mean(u)
+    cy = np.mean(v)
+
+    du = u - cx
+    dv = v - cy
+
+    d = np.sqrt(du**2 + dv**2)
+    mean_d = np.mean(d)
+
+    if mean_d < 1e-12:
+        s = 1.0
+    else:
+        s = np.sqrt(2) / mean_d
+
+    T = np.array([[s, 0, -s * cx],
+                  [0, s, -s * cy],
+                  [0, 0, 1]])
+
     return T
 
 
@@ -38,27 +65,58 @@ def eight_points_algorithm(x1, x2, normalize=True):
 
     if normalize:
         # Construct transformation matrices to normalize the coordinates
+        # TODO:
         T1 = get_normalization_matrix(x1)
         T2 = get_normalization_matrix(x2)
 
         # Normalize inputs
-       
+        # TODO:
+        x1n = T1 @ x1
+        x2n = T2 @ x2
+    else:
+        x1n, x2n = x1, x2
 
     # Construct matrix A encoding the constraints on x1 and x2
-    A = x1.T @ x2
+    # TODO:
+    x1n = x1n / x1n[2, :]
+    x2n = x2n / x2n[2, :]
+
+    u1 = x1n[0, :]
+    v1 = x1n[1, :]
+    u2 = x2n[0, :]
+    v2 = x2n[1, :]
+
+    N = x1.shape[1]
+
+    A = np.column_stack([u2 * u1, 
+                        u2 * v1, 
+                        u2, 
+                        v2 * u1, 
+                        v2 * v1, 
+                        v2, 
+                        u1,
+                        v1,
+                        np.ones(N)])
 
     # Solve for f using SVD
-    f,_ ,_ = svds(A, k=8)
+    # TODO:
+    U, S, Vt = np.linalg.svd(A)
+    f = Vt[-1, :]
+    F = f.reshape(3, 3)
 
     # Enforce that rank(F)=2
-    F,_ ,_ = svds(f.T @ f, k=2)
-    assert np.linalg.matrix_rank(F) == 2
+    # TODO:
+    U_f, S_f, Vt_f = np.linalg.svd(F)
+    S_f[-1] = 0
+    F_norm = U_f @ np.diag(S_f) @ Vt_f
 
 
     if normalize:
-        # Transform F back
-        # TODO
-        pass
+        # Transform F back 
+        # TODO:
+        F = T2.T @ F_norm @ T1
+    else:
+        F = F_norm
 
     return F
 
@@ -71,8 +129,11 @@ def right_epipole(F):
 
     # The epipole is the null space of F (F * e = 0)
     # TODO
-    e = None
+    _,_, Vt = np.linalg.svd(F)
+    e = Vt[-1, :]
 
+    if np.abs(e[2]) > 1e-12:
+        e = e / e[2]
 
     return e
 
@@ -84,22 +145,84 @@ def plot_epipolar_line(im, F, x, e, ax = None):
     """
     m, n = im.shape[:2]
     # TODO
-    
+
     if ax is None:
         ax = plt
-    ax.plot( )#TODO
+
+    a, b, c = F @ x
+
+    ax.imshow(im, cmap="gray")
+
+    if e is not None and np.abs(e[2]) > 1e-12:
+        ex, ey = e[0] / e[2], e[1] / e[2]
+        ax.plot(ex, ey, 'rx')
+
+    if abs(b) > 1e-8:
+        u0, u1 = 0, n - 1
+        v0 = -(c + a * u0) / b
+        v1 = -(c + a * u1) / b
+    elif abs(a) >= 1e-12:
+        u0 = u1 = -c / a
+        v0, v1 = 0, m - 1
+
+    ax.plot([u0, u1], [v0, v1])
+
+
 
 
 def ransac(x1, x2, threshold, num_steps=1000, random_seed=42):
     if random_seed is not None:
         np.random.seed(random_seed)  # we are using a random seed to make the results reproducible
+
     # TODO setup variables
+    assert x1.shape[1] == x2.shape[1]
+    N = x1.shape[1]
+    assert N >= 8, "Need at least 8 correspondences for 8-point algorithm"
+
+    best_num_inliers = 0
+    best_sample_indices = None
+
     for _ in range(num_steps):
-        pass # TODO main loop
+        # TODO main loop
+        idx = np.random.choice(N, 8, replace=False)
+
+        try:
+            F_candidate = eight_points_algorithm(x1[:, idx], x2[:, idx], normalize=True)
+        except np.linalg.LinAlgError:
+            continue
+
+        d = np.abs(np.sum(x2 * (F_candidate @ x1), axis=0))
+
+        inliers_mask = d < threshold
+        num_inliers = np.sum(inliers_mask)
+
+        if num_inliers > best_num_inliers:
+            best_num_inliers = num_inliers
+            best_sample_indices = idx
+
+    if best_sample_indices is None:
+        F = eight_points_algorithm(x1, x2, normalize=True)
+        inliers = np.ones(N, dtype=bool)
+        return F, inliers
+
     # TODO calculate initial inliers with with the best candidate points
+    F_initial = eight_points_algorithm(x1[:, best_sample_indices],
+                                       x2[:, best_sample_indices],
+                                       normalize=True)
+    d_initial = np.abs(np.sum(x2 * (F_initial @ x1), axis=0))
+    initial_inliers = d_initial < threshold
+
     # TODO estimate F with all the inliers
+    F_refined = eight_points_algorithm(x1[:, initial_inliers],
+                                       x2[:, initial_inliers],
+                                       normalize=True)
+    
     # TODO find final inliers with F
-    F, inliers = None, None
+    d_final = np.abs(np.sum(x2 * (F_refined @ x1), axis=0))
+    final_inliers = d_final < threshold
+
+    F, inliers = F_refined, final_inliers
+    
     return F, inliers  # F is estimated fundamental matrix and inliers is an indicator (boolean) numpy array
 
 
@@ -113,8 +236,33 @@ def decompose_essential_matrix(E, x1, x2):
     Rl = np.eye(3)
     tl = np.array([[0, 0, 0]]).T
     Pl = np.concatenate((Rl, tl), axis=1)
-
+    
     # TODO: Compute possible rotations and translations
+    U, S, Vt = np.linalg.svd(E)
+
+    s = (S[0] + S[1]) / 2.0
+    E_hat = U @ np.diag([s, s, 0.0]) @ Vt
+
+    U, S, Vt = np.linalg.svd(E_hat)
+
+    if np.linalg.det(U @ Vt) < 0:
+        Vt = -Vt
+
+    W = np.array([[0, -1, 0],
+                  [1,  0, 0],
+                  [0,  0, 1]])
+
+    R1 = U @ W @ Vt
+    R2 = U @ W.T @ Vt
+
+    if np.linalg.det(R1) < 0:
+        R1 = -R1
+    if np.linalg.det(R2) < 0:
+        R2 = -R2
+
+    t = U[:, 2].reshape(3, 1)
+    t1 = t
+    t2 = -t
 
     # Four possibilities
     Pr = [np.concatenate((R1, t1), axis=1),
